4a321a214ae0f46e49227903457af9a61aef8b87
TimurL10 <tlumelsky@gmail.com>
2020-03-10 21:23:35 +0300

    full proj

---
+++ CalculatingGeometricShapes.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatingGeometricShapes.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CalculatingGeometricShapes.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14d7c236-37e0-418f-8aba-90ce1d074252")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalculatingGeometricShapes.UnitTests/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatingGeometricShapes.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+                
+        [TestMethod]
+        public void ReturnCircleSquere()
+        {
+            //Arrange
+            var shapes = new Shapes();
+
+            //Act
+            var result = shapes.Circle(4);
+
+            //Assert
+
+            Assert.AreEqual(16 * Math.PI, result);
+        }
+        [TestMethod]
+        public void ReturnTriangleSquare()
+        {
+            //Arrange
+            var shapes = new Shapes();
+
+            //Act
+            var result = shapes.Triangle(14, 19, 18);
+
+            //Assert
+            Assert.AreEqual(119.566, result);
+        }
+        [TestMethod]
+        public void CheckIfTriangleRightFalse()
+        {
+            //Arrange
+            var shapes = new Shapes();
+
+            //Act
+            var result = shapes.TriangleCheck(5, 11, 8);
+
+            //Assert
+            Assert.IsFalse(false);
+        }
+        [TestMethod]
+        public void CheckIfTriangleRightTrue()
+        {
+            //Arrange
+            var shapes = new Shapes();
+
+            //Act
+            var result = shapes.TriangleCheck(5, 13, 12); //3, 4, 5; 6, 8, 10; 12, 16, 20; 5, 12, 13.
+
+            //Assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void CheckAutoShapesDetectIfItCircle()
+        {
+            //Arrange
+            var shapes = new Shapes();
+
+            //Act
+            var result = shapes.AutoShapesDetect(2);
+
+            //Assert
+            Assert.AreEqual(4 * Math.PI, result);
+        }
+        [TestMethod]
+        public void CheckAutoShapesDetectIfItTriangle()
+        {
+            //Arrange
+            var shapes = new Shapes();
+
+            //Act
+            var result = shapes.AutoShapesDetect(4, 9, 8);
+
+            //Assert
+            Assert.AreEqual(15.998, result);
+        }
+    }
+}
+    
+
